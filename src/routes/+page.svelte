5c1c37417ac4e97a5aad32281d8f9629194d4a1b
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-26 17:07:42 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,5 @@
 	Title="Home"
 	Description="Onlyfoodz is a social media platform where people share pictures and small videos of food."
 />
+
+<h2 class="text-white font-semibold text-2xl">Coming Soon!</h2>
