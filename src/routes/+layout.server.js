6c04deffe54fb6bd81db75dae263a63dccd5e463
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-26 16:58:41 -0600

    Update +layout.server.js

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -3,14 +3,6 @@ import cookie from 'cookie';
 export const load = async ({ request, setHeaders }) => {
 	const cookies = cookie.parse(request.headers.get('cookie') || '');
 
-	const currentlyPlaying = await fetch('https://api.nightmarebot.tk/api/spotify/playing').then(
-		(res) => res.json()
-	);
-
-        const recentlyPlayed = await fetch('https://api.nightmarebot.tk/api/spotify/recent').then(
-		(res) => res.json()
-	);
-
         if (cookies.token) {
 		const userData = await fetch(
 			`https://api.nightmarebot.tk/api/users/getwithtoken?token=${cookies.token}`
@@ -22,20 +14,14 @@ export const load = async ({ request, setHeaders }) => {
 
 		if (userData.error)
 			return {
-				user: null,
-				currentlyPlaying: currentlyPlaying,
-                                recentlyPlayed: recentlyPlayed
+				user: null
 			};
 		else
 			return {
-				user: userData,
-				currentlyPlaying: currentlyPlaying,
-                                recentlyPlayed: recentlyPlayed
+				user: userData
 			};
 	} else
 		return {
-			user: null,
-			currentlyPlaying: currentlyPlaying,
-                        recentlyPlayed: recentlyPlayed
+			user: null
 		};
 };
