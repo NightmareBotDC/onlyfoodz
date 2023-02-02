0f113e077eb2d943e94b8c6b8e82def48901430d
Yeji <select@nightmarebot.tk>
2023-02-01 18:31:03 -0600

    Added legal stuff

---
+++ src/routes/legal/+page.svelte
@@ -0,0 +1,12 @@
+<h2 class="text-white font-bold text-2xl">Legal</h2>
+<h6 class="text-gray-400">This page provides some links to our legal policy pages such as our Terms of Service.</h6>
+
+<div class="p-4"></div>
+
+<div id="legal">
+   <a class="text-red-600 font-bold" href="/legal/terms">Terms of Service</a>
+   <div class="p-2"></div>
+   <a class="text-red-600 font-bold" href="/legal/uselicense">Use License</a>
+   <div class="p-2"></div>
+   <a class="text-red-600 font-bold" href="/legal/sourcelicense">Source License</a>
+</div>
