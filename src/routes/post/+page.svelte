594d7f8716698971a848e43f99202c8bff52e583
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 14:38:37 -0600

    Create +page.svelte

---
+++ src/routes/post/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+   import Meta from '../components/Meta.svelte';
+</script>
+
+<Meta
+   Title="Post Something"
+   Description="Onlyfoodz is a social media platform where people share pictures and small videos of food."
+/>
+
+<h2 class="text-white">This page is coming soon.</h2>
