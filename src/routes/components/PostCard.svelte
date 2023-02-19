6c4e0f2643685552e50fa35bc69364f9cb6a4c68
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 11:40:14 -0600

    Create PostCard.svelte

---
+++ src/routes/components/PostCard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+   export let User: Object;
+   export let Caption: String:
+   export let Image: String;
+   export let CreatedAt: Date;
+</script>
+
+<h2>{Caption}</h2>
+<h2>{Image || "No image provided"}</h2>
+<h4>{CreatedAt}</h4>
