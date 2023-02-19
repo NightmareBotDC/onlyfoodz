b506750f2ab9bb74a009b1f2be1b9cbb427aaef4
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 13:45:24 -0600

    Update PostCard.svelte

--- src/routes/components/PostCard.svelte
+++ src/routes/components/PostCard.svelte
@@ -3,9 +3,10 @@
    export let Caption: String;
    export let Image: String;
    export let CreatedAt: Date;
+   export let PostId: String;
 </script>
 
-<a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<a href="/post/{PostId}" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h2
 	class="flex"
     >
