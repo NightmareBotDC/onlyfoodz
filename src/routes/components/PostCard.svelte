78fde78105eeeeda3f7f63e26d9092a79a1208cb
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 12:45:43 -0600

    Update PostCard.svelte

--- src/routes/components/PostCard.svelte
+++ src/routes/components/PostCard.svelte
@@ -5,7 +5,7 @@
    export let CreatedAt: Date;
 </script>
 
-<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<a href="#" class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h2
 	class="flex"
     >
