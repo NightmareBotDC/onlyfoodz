598fb24e04023e9038ccddda97097448cf9a01f2
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 14:44:13 -0600

    Update +page.svelte

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -15,7 +15,7 @@
 	<div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
 		<div class="max-w-md text-center">
 			<h2 class="mb-8 font-extrabold text-9xl dark:text-gray-600">
-				<span class="sr-only">Error</span>000
+				<span class="sr-only">Error</span>404
 			</h2>
 			<p class="text-2xl font-semibold md:text-3xl">Sorry, that post does not exist here in this universe.</p>
 			<p class="mt-4 mb-8 dark:text-gray-400">But, don't worry there are other posts available here!</p>
