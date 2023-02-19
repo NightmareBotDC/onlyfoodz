d6a31c2ada7152cd304414729c6ab093a129d86d
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 14:42:30 -0600

    Update +page.svelte

--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
    import Meta from '../components/Meta.svelte';
+
+   export let data;
 </script>
 
 <Meta
@@ -7,4 +9,8 @@
    Description="Onlyfoodz is a social media platform where people share pictures and small videos of food."
 />
 
-<h2 class="text-white">This page is coming soon.</h2>
+{#if data.user}
+   <h2 class="text-white">This page is coming soon.</h2>
+{:else}
+   <h2 class="text-white">You are not logged in.</h2>
+{/if}
