e2f9226a6110b2d1b3740490ea7ca71ac39e37bd
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 13:55:10 -0600

    Update +page.svelte

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -5,6 +5,11 @@
 </script>
 
 {#if data.post}
+   <Meta
+	Title="Post"
+	Description="`{data.post.post.Caption}` - {data.post.user.Username}"
+   />
+
    <PostCard User={data.post.user} Caption={data.post.post.Caption} Image={data.post.post.Image} CreatedAt={data.post.post.CreatedAt} PostID={data.post.post.PostID} />
 {:else}
    <h2 class="text-white">Sorry, that post does not exist in this universe!</h2>
