0399c282e2967f4b843d4f768d654b14b2c796e1
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 19:40:21 -0600

    Basic profile stuff

--- src/routes/profile/[slug]/+page.svelte
+++ src/routes/profile/[slug]/+page.svelte
@@ -12,5 +12,9 @@
 {#if !data.profile || data.profile.error}
     <h2 class="text-white">Sorry, that profile cannot be found.</h2>
 {:else}
-    <h2 class="text-white">{JSON.stringify(data.profile)}</h2>
+    <h2 class="text-white">Username: {data.profile.Username}</h2>
+    <h2 class="text-white">UserID: {data.profile.UserID}</h2>
+    <h2 class="text-white">Bio: {data.profile.Bio || "None"}</h2>
+    <h2 class="text-white">Account Creation Date: {new Date(data.profile.CreatedAt)}</h2>
+    <img class="rounded-full" src="https://cdn.discordapp.com/avatars/{data.profile.UserID}/{data.profile.Avatar}" height="120px" width="120px" />
 {/if}
