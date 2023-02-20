57a749ad353b3c427e9d5444bf94d0df1a31b859
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 20:04:08 -0600

    Update PostCard.svelte

--- src/routes/components/PostCard.svelte
+++ src/routes/components/PostCard.svelte
@@ -6,11 +6,9 @@
 	export let PostID: string;
 </script>
 
-<a
-	href="/post/{PostID}"
-	class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
->
-	<h2 class="flex">
+<div class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+       <a href="/profile/{User.UserID}">
+       <h2 class="flex">
 		<img
 			class="h-8 rounded-full"
 			src="https://cdn.discordapp.com/avatars/{User.UserID}/{User.Avatar}"
@@ -20,7 +18,8 @@
 		/>
 		<p class="ml-2 mt-1 mb-1 font-normal text-gray-700 dark:text-gray-400">{User.Username}</p>
 	</h2>
-
+      </a>
+     <a href="/post/{PostID}">
 	<div class="p-2" />
 
 	{#if Image}
@@ -29,6 +28,7 @@
 
 	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{Caption}</h5>
 	<p class="font-normal text-gray-700 dark:text-gray-400">{new Date(CreatedAt)}</p>
-</a>
+    </a>
+</div>
 
 <div class="p-2" />
