e8adeb5b2e05101940850a113a96b532e556faee
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-20 09:08:23 -0600

    Update +page.svelte

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -61,7 +61,7 @@
                     <div class="p-2"></div>
 
                     <div id="profile_actions">
-                        <button>Edit Profile</button>
+                        <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit Profile</button>
                     </div>
                 {/if}
 	</div>
