67a10d9ef5f544fdd072669ca7839b189fbd802e
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-19 23:53:18 -0600

    Update +page.svelte

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -43,7 +43,7 @@
 				width="60px"
 				alt="Profile Picture"
 			/>
-			<p class="ml-2 mt-1 mb-1 font-bold text-xl text-white">
+			<p class="ml-2 font-bold text-xl text-white">
 				{data.user.Username}
 			</p>
 		</h2>
