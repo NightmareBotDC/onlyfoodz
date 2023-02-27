981fccb100e3767c25090a53517e09784c72299b
selectdev <66577268+selectdev@users.noreply.github.com>
2023-02-27 17:44:08 -0600

    Update PostCard.svelte

--- src/routes/components/PostCard.svelte
+++ src/routes/components/PostCard.svelte
@@ -9,7 +9,7 @@
 <div
 	class="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
 >
-	<a href="/profile/{User.UserID}">
+	<a href="/{User.team ? 'team' : 'profile'}/{User.UserID}">
 		<h2 class="flex">
 			<img
 				class="h-8 rounded-full"
