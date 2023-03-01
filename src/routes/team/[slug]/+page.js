a055539fe56e0a919fddf9bf5e31d111925cb9ce
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-01 17:10:30 -0600

    Create +page.js

---
+++ src/routes/team/[slug]/+page.js
@@ -0,0 +1,34 @@
+/** @type {import('./$types').PageLoad} */
+export async function load({ params }) {
+	const team = await fetch(`https://api.nightmarebot.tk/api/teams/get?id=${params.slug}`).then(
+		(res) => {
+			const status = res.status;
+
+			if (status === 200) return res.json();
+			else
+				return {
+					error: 'Unable to reach server.'
+				};
+		}
+	);
+
+	let teamPosts;
+	if (!team || team.error) teamPosts = null;
+	else
+		teamPosts = await fetch(
+			`https://api.nightmarebot.tk/api/posts/list_user?user_id=${profile.UserID}&type=1`
+		).then((res) => {
+			const status = res.status;
+
+			if (status === 200) return res.json();
+			else
+				return {
+					error: 'Unable to reach server.'
+				};
+		});
+
+	return {
+		team: team,
+		teamPosts: teamPosts
+	};
+}
