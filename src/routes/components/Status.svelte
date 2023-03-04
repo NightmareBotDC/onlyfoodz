e87625a42867ef85f011bb4aa225807b449173cc
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-04 09:44:47 -0600

    Update Status.svelte

--- src/routes/components/Status.svelte
+++ src/routes/components/Status.svelte
@@ -1,1 +1,5 @@
-<script></script>
+<script lang="ts">
+   const data = fetch("https://nightmare-project.instatus.com/summary.json").then(async (res) => { return await res.json(); });
+</script>
+
+<h2 class="text-white font-bold">{JSON.stringify(data)}</h2>
