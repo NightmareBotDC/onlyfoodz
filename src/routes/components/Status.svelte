970f64ea2c8125065c7c7e2463c06d7f6a049ad3
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-04 10:10:24 -0600

    Update Status.svelte

--- src/routes/components/Status.svelte
+++ src/routes/components/Status.svelte
@@ -6,4 +6,6 @@
    {#if data.status.page.status === "UP"}
        <h2 class="text-green-700 font-bold">All services are online!</h2>
    {/if}
+{:else}
+   <h2 class="text-red-800 font-bold">Unable to reach status information.</h2>
 {/if}
