bde96381988ea13e8469fdd7c0307bbd46b58867
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-04 11:10:06 -0600

    Update Status.svelte

--- src/routes/components/Status.svelte
+++ src/routes/components/Status.svelte
@@ -16,6 +16,4 @@
           <h2 class="text-white font-bold text-center">Some services are under maintenance!</h2>
       {/if}
    </div>
-{:else}
-   <h2 class="text-red-800 font-bold">Unable to reach status information.</h2>
 {/if}
