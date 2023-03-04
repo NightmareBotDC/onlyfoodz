3661b429ec57055e69fcc4c667bc91f39941651b
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-04 10:39:13 -0600

    Update Status.svelte

--- src/routes/components/Status.svelte
+++ src/routes/components/Status.svelte
@@ -6,6 +6,14 @@
    {#if data.page.status === "UP"}
        <h2 class="text-green-700 font-bold">All services are online!</h2>
    {/if}
+ 
+   {#if data.page.status === "HASISSUES"}
+       <h2 class="text-red-800 font-bold">Some services are having issues!</h2>
+   {/if}
+
+   {#if data.page.status === "UNDERMAINTENANCE"}
+       <h2 class="text-white font-bold">Some services are under maintenance!</h2>
+   {/if}
 {:else}
    <h2 class="text-red-800 font-bold">Unable to reach status information.</h2>
 {/if}
