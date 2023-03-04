08de35448a895732a9408629bf2d115c04c639d6
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-04 10:27:56 -0600

    Update Status.svelte

--- src/routes/components/Status.svelte
+++ src/routes/components/Status.svelte
@@ -2,8 +2,8 @@
    export let data: any;
 </script>
 
-{#if data.status}
-   {#if data.status.page.status === "UP"}
+{#if data}
+   {#if data.page.status === "UP"}
        <h2 class="text-green-700 font-bold">All services are online!</h2>
    {/if}
 {:else}
