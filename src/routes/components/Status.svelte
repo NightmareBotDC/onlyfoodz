b4e1547f2331adf1a796cb94dce6e55da440f3ba
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-04 11:06:55 -0600

    Update Status.svelte

--- src/routes/components/Status.svelte
+++ src/routes/components/Status.svelte
@@ -3,17 +3,17 @@
 </script>
 
 {#if data}
-   <div class="h-14 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md">
+   <div class="p-6 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md">
       {#if data.page.status === "UP"}
-          <h2 class="text-green-700 font-bold">All services are online!</h2>
+          <h2 class="text-green-700 font-bold text-center">All services are online!</h2>
       {/if}
  
       {#if data.page.status === "HASISSUES"}
-          <h2 class="text-red-800 font-bold">Some services are having issues!</h2>
+          <h2 class="text-red-800 font-bold text-center">Some services are having issues!</h2>
       {/if}
 
       {#if data.page.status === "UNDERMAINTENANCE"}
-          <h2 class="text-white font-bold">Some services are under maintenance!</h2>
+          <h2 class="text-white font-bold text-center">Some services are under maintenance!</h2>
       {/if}
    </div>
 {:else}
