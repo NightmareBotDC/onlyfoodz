42b181572b73d54563108613a733c2fb51387395
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-04 22:12:18 -0600

    Removed the coming soon thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,6 @@
 	Description="Onlyfoodz is a social media platform where people share pictures and small videos of food."
 />
 
-<h2 class="text-white font-semibold text-2xl">Coming Soon!</h2>
-<div class="p-6" />
-
 {#if !data.posts || data.posts.error}
 	<h2 class="text-white font-bold text-3xl">
 		It seems that our servers are currently experiencing a little too much brain damage at this
