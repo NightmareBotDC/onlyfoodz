55caec216259501beb0b621ff805c8a24863b749
selectdev <66577268+selectdev@users.noreply.github.com>
2023-03-06 14:53:02 -0600

    Update +page.svelte

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -13,7 +13,7 @@
 	};
 
 	const loginDiscord = async () => {
-		window.location.href = "https://api.nightmarebot.tk/auth/login";
+		window.location.href = "https://api.nightmarebot.tk/auth/login?client_id=onlyfoodz-0091";
 	};
 
 	export let data: any;
